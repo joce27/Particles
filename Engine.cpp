69902a588f144d335583888cec925e8b83973725
Your Name <you@example.com>
2024-04-26 18:05:53 -0700

    update

---
+++ Engine.cpp
@@ -0,0 +1,26 @@
+#include "Engine.h"
+
+void Engine::input()
+{
+
+}
+
+void Engine::update(float dtAsSeconds)
+{
+
+}
+
+void Engine::draw()
+{
+
+}
+
+Engine::Engine()
+{
+
+}
+
+void Engine::run()
+{
+
+}
